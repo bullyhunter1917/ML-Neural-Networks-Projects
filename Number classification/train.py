45b0945e1e1271b5bfccc596e920ee368b3f3231
ATTENSHONE <michamroz77@gmail.com>
2023-03-21 21:10:13 +0100

    Added initing model

--- Number classification/train.py
+++ Number classification/train.py
@@ -1,8 +1,13 @@
+import time
+
+from torch import nn
+from torch.optim import Adam
 from torch.utils.data import random_split
 from torchvision.transforms import ToTensor
 from torchvision.datasets import KMNIST
+from torch.utils.data import DataLoader
 import torch
-
+from lenet import lenet
 
 # initing training parameters
 INIT_LR = 1e-3
@@ -22,4 +27,27 @@ _test = KMNIST(root='data', train=False, download=True, transform=ToTensor())
 trainSampleSize = int(len(_train) * TRAIN_SPLIT)
 valSampleSize = int(len(_train) * VAL_SPLIT)
 (_train, _val) = random_split(_train, [trainSampleSize, valSampleSize], generator=torch.Generator().manual_seed(42))
+
+# Creating dataloaders for train val and test datasets
+_trainDataLoader = DataLoader(_train, batch_size=BATCH_SIZE, shuffle=True)
+_valDataLoader = DataLoader(_val, batch_size=BATCH_SIZE)
+_testDataLoader = DataLoader(_test, batch_size=BATCH_SIZE)
+
+_trainStep = len(_train) // BATCH_SIZE
+_valStep = len(_val) // BATCH_SIZE
+
+model = lenet.LeNet(numOfChannels=1, classes=len(_train.dataset.classes)).to(device)
+
+optimizer = Adam(model.parameters(), lr=INIT_LR)
+lossFunction = nn.NLLLoss()
+
+# Creating history to make plots
+History = {
+    "train_loss": [],
+    "train_acc": [],
+    "val_loss": [],
+    "val_acc": []
+}
+
+
 
