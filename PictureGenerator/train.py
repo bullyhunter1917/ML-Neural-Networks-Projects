03c81b559e7e103ff36e6570fb741b0fa08ab4dd
Michał Mróz <michamroz77@gmail.com>
2023-03-28 23:11:08 +0200

    Added forward function

--- PictureGenerator/train.py
+++ PictureGenerator/train.py
@@ -2,13 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
-def forward(x, mean, sigma):
-    return x + (255*np.random.normal(mean, sigma, x.shape)).astype(int)
+BETA_start = 0.0001
+BETA_end = 0.02
+T = 1000
+
+beta_t = np.linspace(BETA_start, BETA_end, T)
+one_minus_beta_t = np.linspace((1 - BETA_start), (1 - BETA_end), 1000)
+
+
+# Whole forward in one step
+def forward(x):
+    return np.sqrt(np.prod(one_minus_beta_t))*x + np.sqrt(1 - np.prod(one_minus_beta_t))*np.random.normal(0, 1, x.shape)
+
+
+# One forward step
+def forward_one_step(x, t):
+    return np.sqrt(one_minus_beta_t[t])*x + np.sqrt(beta_t[t])*np.random.normal(0, 1, x.shape)
+
 
 originalPicture = plt.imread('Mallard.jpg')
 
-
-
-
-plt.imshow(forward(originalPicture, 0, 0.5))
-plt.show()
+# Normalizing picture
+originalPicture = originalPicture.astype(float)
+originalPicture /= 255
